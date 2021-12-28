21e10369384905837efde0e4c60499b711aed220
Marcus1113 <58456045+Marcus1113@users.noreply.github.com>
2021-12-28 22:59:51 +0800

    + finish

---
+++ components/Jumbotron/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useRouter } from "next/router"
+
+export default function Jumbotron() {
+    
+    const router = useRouter();
+    
+    return (
+        <div className="jumbotron jumbotron-fluid mt-5">
+        <div className="container">
+            <h1 className="display-4">Cats World</h1>
+            <p className="lead">Find your new best friend today. Adopt a wonderful cat</p>
+            <button type="button" className="btn btn-primary" 
+            onClick={() => router.push("/cats")}>Start looking</button>
+        </div>
+    </div>
+    )
+}
